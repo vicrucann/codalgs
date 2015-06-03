e91134bf0cc667ac319f8fa9f2ad15fc05f4cef5
Victoria Rudakova <vicrucann@gmail.com>
2015-06-02 23:22:12 -0400

    t9 draft

--- kattis/adhoc.cpp
+++ kattis/adhoc.cpp
@@ -44,7 +44,7 @@ void toilet(){
     int p2=0;
     int p3=0;
 
-    for (int i=0;i<line.length();++i){
+    for (unsigned int i=0;i<line.length();++i){
         if (i>0){
             if (line[i]=='U')
                 p2+=2;
@@ -72,8 +72,48 @@ void toilet(){
     printf("%i\n%i\n%i\n", p1,p2,p3);
 }
 
+string keys[12] = {
+    "","abc","def",
+    "ghi","jkl","mno",
+    "pqrs","tuv","wxyz",
+    ""," ",""};
+
+void t9(){
+    int n;
+    scanf("%d\n",&n);
+    int x=1;
+    string out;
+    int out = -1;
+    while(n--){
+        string line;
+        getline(cin, line);
+
+        printf("Case #%d: ", x);
+        for (unsigned int i=0;i<line.length();++i){
+            for (unsigned int j=0;j<12;++j){
+                for (unsigned int k=0;k<keys[j].size();++k){
+                    if (line[i] == keys[j][k]){
+                        int test = j==10? 0 : j+1;
+                        if (test==out){
+                            printf(" ");
+                        }
+                        for (unsigned int p=0;p<=k;++p){
+                            out = j==10? 0 : j+1;
+                            printf("%d", out);
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+        printf("\n");
+        x++;
+    }
+}
+
 int main(){
     //eightqueens();
-    toilet();
+    //toilet();
+    t9();
     return 0;
 }
