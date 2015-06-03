4ea7bb1c6b268b246fb9c6848428850c2a1954bc
Victoria Rudakova <vicrucann@gmail.com>
2015-06-02 20:01:10 -0400

    initial

---
+++ kattis/adhoc.cpp
@@ -0,0 +1,79 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+void eightqueens(){
+    int w=8;
+    string line;
+    bool a[w][w];
+    int qis[8], qjs[8];
+    int k=0;
+    for (int i=0;i<w;++i) {
+        getline(cin,line);
+        for (int j=0;j<w;++j){
+			a[i][j]=(line[j]=='*');        
+            if (a[i][j]){
+                qis[k]=i;
+                qjs[k]=j;
+                k++;
+            }
+        }
+    }
+
+    for (int k=0;k<w;++k){
+        for (int l=0; l<w; ++l){
+            if (k==l)
+                continue;
+            if (qis[k]==qis[l] || qjs[k]==qjs[l] || abs(qis[k]-qis[l])==abs(qjs[k]-qjs[l])){
+                printf("invalid\n");
+                return;
+            }
+        }
+    }
+    printf("valid\n");
+}
+
+void toilet(){
+    string line;
+    getline(cin,line);
+    int p1=0;
+    int p2=0;
+    int p3=0;
+
+    for (int i=0;i<line.length();++i){
+        if (i>0){
+            if (line[i]=='U')
+                p2+=2;
+            else
+                p1+=2;
+            if (line[i]!=line[i-1])
+                p3++;
+        }
+        else {
+            if (line[0]==line[1] && line[0]=='U'){
+                p2--;
+            }
+            if (line[0]==line[1] && line[0]=='D'){
+                p1--;
+            }
+            if (line[0]!=line[1] && line[0]=='U'){
+                p2++;
+            }
+            if (line[0]!=line[1] && line[0]=='D'){
+                p1++;
+            }
+
+        }
+    }
+    printf("%i\n%i\n%i\n", p1,p2,p3);
+}
+
+int main(){
+    //eightqueens();
+    toilet();
+    return 0;
+}
