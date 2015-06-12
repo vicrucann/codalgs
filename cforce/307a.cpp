9420ea5d13bfdbdd89563f312ee3c07708005990
Victoria Rudakova <vicrucann@gmail.com>
2015-06-12 15:00:52 -0400

    drafts

--- cforce/307a.cpp
+++ cforce/307a.cpp
@@ -8,10 +8,55 @@
 #include <cstdlib>
 #include <cassert>
 #include <string>
+#include <utility>
 
 using namespace std;
 
+bool foo(const pair<int,int>& i, const pair<int,int>& j){
+    return ((i.first > j.first));
+}
+
 int main(){
     cerr << "Check : 307a" << endl;
+
+    int n;
+    cin >> n;
+    vector<pair <int,int> > a;
+    int res[n];
+    for (int i=0;i<n;++i){
+        int c;
+        cin >> c;
+        res[i] = c;
+        a.push_back(make_pair(c,i));
+    }
+    sort(a.begin(), a.end(), foo);
+
+    int sc_p=1, p_p=1;
+    int resc[n];
+    for (int i=0;i<n;++i){
+        if (i==0){
+            sc_p=a[i].first;//res[a[i].second];
+            p_p = 1;
+            resc[a[i].second]=p_p;
+        }
+        else {
+            int sc = a[i].first;//res[a[i].second];
+            if (sc == sc_p){
+                resc[a[i].second]=p_p;
+            }
+            else{
+                p_p=i+1;
+                sc_p = a[i].first; //res[a[i].second];
+                resc[a[i].second]=p_p;
+            }
+        }
+    }
+    for (int i=0;i<n;++i){
+        if (i==0)
+            cout << resc[i];
+        else
+            cout << " " << resc[i];
+    }
+
     return 0;
 }
--- cforce/307c.cpp
+++ cforce/307c.cpp
@@ -11,7 +11,34 @@
 
 using namespace std;
 
+struct Tri{
+    int x,y,z;
+};
+
+bool cmp(const Tri& i, const Tri& j){
+    return ((i.z > j.z));
+}
+
 int main(){
     cerr << "Check 307c" << endl;
+    int n,m;
+    cin >> n >> m;
+    //vector<Tri> a;
+    int a[n];
+    int sum=0;
+    for (int i = 0; i < n; ++i){
+        int w;
+        cin >> w;
+        //a.push_back((w,i,w*i));
+        a[i]=w;
+        sum+=w;
+    }
+    //sort(a.begin(), a.end(), cmp);
+    int t=0;
+    for (int i=0;i<n;i++){
+        t+=i+1+(a[i]);
+    }
+
+
     return 0;
 }
