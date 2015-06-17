f73c3a16762f8754713662a7279855c3d5a73e46
Victoria Rudakova <vicrucann@gmail.com>
2015-06-17 16:22:45 -0400

    +uva -kattis

---
+++ uva/template.cpp
@@ -0,0 +1,17 @@
+#include <cstring>
+#include <vector>
+#include <list>
+#include <algorithm>
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <cassert>
+#include <string>
+
+using namespace std;
+
+int main(){
+    cerr << "Hello World" << endl;
+    return 0;
+}
