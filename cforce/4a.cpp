f1ca15669df851c438b43460d0e4d291e6dc8379
Victoria Rudakova <vicrucann@gmail.com>
2015-07-13 11:52:38 -0400

    1 liner

---
+++ cforce/4a.cpp
@@ -0,0 +1,21 @@
+#include <cstring>
+#include <vector>
+#include <list>
+#include <algorithm>
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <cassert>
+#include <string>
+#include <queue>
+#include <set>
+
+using namespace std;
+
+int main(){
+    int w;
+    scanf("%d", &w);
+    (w%2==0 && w>2)? printf("YES") : printf("NO");
+    return 0;
+}
