2d4c08989a2032c2f2f36bd484460d0a226d0198
Victoria Rudakova <vicrucann@gmail.com>
2015-07-13 15:37:34 -0400

    surface square, greedy, math

---
+++ cforce/50a.cpp
@@ -0,0 +1,21 @@
+#include <cstring>
+#include <vector>
+#include <list>
+#include <algorithm>
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <cassert>
+#include <string>
+#include <queue>
+#include <set>
+
+using namespace std;
+
+int main(){
+    int m,n;
+    scanf("%d %d", &m, &n);
+    printf("%d\n", (m*n)/2);
+    return 0;
+}
