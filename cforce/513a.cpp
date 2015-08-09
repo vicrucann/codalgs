07f53b5c4c6f4eb4e3af66d3213024eeb1f199b4
Victoria Rudakova <vicrucann@gmail.com>
2015-08-08 23:50:22 -0400

    easy, compare, optimal

---
+++ cforce/513a.cpp
@@ -0,0 +1,24 @@
+#include <cstring>
+#include <vector>
+#include <list>
+#include <algorithm>
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <cassert>
+#include <string>
+#include <queue>
+#include <set>
+
+using namespace std;
+
+int main(){
+    int n1,n2,k2,k1;
+    cin>>n1>>n2>>k1>>k2;
+    if (n1>n2)
+        printf("First\n");
+    else
+        printf("Second\n");
+    return 0;
+}
